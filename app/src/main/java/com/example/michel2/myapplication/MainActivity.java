45d13413c842125588812ea6bcce0da6ae654801
JoKing94 <joseph94.king@yahoo.com>
2020-06-30 19:08:09 +0200

    test maison

--- app/src/main/java/com/example/michel2/myapplication/MainActivity.java
+++ app/src/main/java/com/example/michel2/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+	//commentaire à a con
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
